846c5d53b8e1dd235ead104cd9985f95d03d67d8
Ausha Homer <Aushahomer@weber.mail.edu>
2025-07-31 11:18:35 -0600

    End of video 1? Maybe 2. lol

--- ChainedHashtable.h
+++ ChainedHashtable.h
@@ -1,5 +1,58 @@
+#ifndef CHAINEDHASHTABLE_H
+#define CHAINEDHASHTABLE_H
+
 #include <iostream>
 
+template<class Type>
+struct Node{
+    Type info;
+    Node<Type>*link;
+};
+
+template<class Type>
+class ChainedHashtable;
+
+template<class Type>
+std::ostream& operator<<(std::ostream& out, ChainedHashtable<Type> & t);
+
+
+template<class Type>
+class ChainedHashtable{
+
+    public:
+        ChainedHashtable(int max);// Constructor for the Hashtable.  Creates an empty list
+
+
+        ~ChainedHashtable(); //Deconstructor for the Hashtable.  Deallocates memory 
+
+
+        void add(Type item);//Adds to the hashtable
+
+
+        bool remove(Type item);//Removes an item from the hashtable.  Returns true if the item was successfully removed
+
+
+        bool contains(Type item);//Determines if the value is in the hashtable
+
+
+        bool empty();//Determines if the hashtable is empty
+
+
+        int size();//Determines the number of items in the hashtable
+
+
+        friend std::ostream& operator<<(std::ostream& out, ChainedHashtable<Type> & t);//Returns an output stream for displaying the Hashtable
+
+
+
+    protected:
+
+    int sz;
+    int capacity; // size of hashtable
+    Node<Type>** htable;
+
+
+};
 /*
 ChainedHashtable(int max);// Constructor for the Hashtable.  Creates an empty list
 ~ChainedHashtable(); //Deconstructor for the Hashtable.  Deallocates memory 
@@ -10,3 +63,5 @@ bool empty();//Determines if the hashtable is empty
 int size();//Determines the number of items in the hashtable
 std::ostream& operator<<(std::ostream& out, ChainedHashtable<Type> & t);//Returns an output stream for displaying the Hashtable
 */
+
+#endif // CHAINEDHASHTABLE_H
