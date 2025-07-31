216b4ad805f1188bdb74c1e5cc4c490868691a25
AuHomie <143470710+AuHomie@users.noreply.github.com>
2025-07-31 11:02:10 -0600

    Create ChainedHashtable.h
    Added functions that are to be made.

---
+++ ChainedHashtable.h
@@ -0,0 +1,12 @@
+
+
+/*
+ChainedHashtable(int max);// Constructor for the Hashtable.  Creates an empty list
+~ChainedHashtable(); //Deconstructor for the Hashtable.  Deallocates memory 
+void add(Type item);//Adds to the hashtable
+bool remove(Type item);//Removes an item from the hashtable.  Returns true if the item was successfully removed
+bool contains(Type item);//Determines if the value is in the hashtable
+bool empty();//Determines if the hashtable is empty
+int size();//Determines the number of items in the hashtable
+std::ostream& operator<<(std::ostream& out, ChainedHashtable<Type> & t);//Returns an output stream for displaying the Hashtable
+*/
