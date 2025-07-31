b4bbc545315c8ee9cd04f7f08917cb361d63b373
Ausha Homer <Aushahomer@weber.mail.edu>
2025-07-31 11:33:16 -0600

    on to the next video

--- ChainedHashtable.h
+++ ChainedHashtable.h
@@ -2,6 +2,7 @@
 #define CHAINEDHASHTABLE_H
 
 #include <iostream>
+#include <iomanip>
 
 template<class Type>
 struct Node{
@@ -53,10 +54,39 @@ class ChainedHashtable{
 
 
 };
-/*
+
+    template<class Type>
+    ChainedHashtable<Type>::ChainedHashtable(int max){
+        sz = 0;
+        capacity = max;
+        htable = new Node<Type>*[capacity]();
+    };
+
+    template<class Type>
+    ChainedHashtable<Type>::~ChainedHashtable(){
+        delete[] htable;
+    };
+
+    template<class Type>
+    bool ChainedHashtable<Type>::empty(){
+        return sz == 0;
+    };
+
+    template<class Type>
+    int ChainedHashtable<Type>::size(){
+            return sz;
+    };
+
+    template<class Type>
+    void add(Type item){
+        int key = item % capacity;
+
+        if(ChainedHashtable[key])
+    };//Adds to the hashtable
+
+    /*
 ChainedHashtable(int max);// Constructor for the Hashtable.  Creates an empty list
 ~ChainedHashtable(); //Deconstructor for the Hashtable.  Deallocates memory 
-void add(Type item);//Adds to the hashtable
 bool remove(Type item);//Removes an item from the hashtable.  Returns true if the item was successfully removed
 bool contains(Type item);//Determines if the value is in the hashtable
 bool empty();//Determines if the hashtable is empty
